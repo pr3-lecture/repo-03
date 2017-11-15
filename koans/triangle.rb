bbc5a59cfd6ab25f79e5570a1715d725a397f60e
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2017-11-15 01:01:27 +0100

    180
    Beim Dart ganz geil

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+  if a==b && b==c
+     :equilateral
+    elsif a==b || b==c || c==a
+      :isosceles
+    else
+      :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
