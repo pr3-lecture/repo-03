9a9a8d179bdc004e19d7772e1546c24d31913353
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2018-01-03 11:32:41 +0100

    Spaß mit C

--- crypter/main.c
+++ crypter/main.c
@@ -1,55 +1,80 @@
-#include "crypto.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+#include "crypto.h"
 
 char* copyString(const char* input, size_t length)
 {
-    char* copy = NULL;
-    copy = (char*) malloc ((length +1)* sizeof(char));
+    char* destination = NULL;
+    destination = (char*) malloc((length + 1) * sizeof(char));
 
-    if(!copy)
+    if(!destination)
     {
         return NULL;
     }
-    if(!strncpy(copy, input, length))
+
+    if(!strncpy(destination, input, length))
     {
         return NULL;
     }
-    copy[length] = '\0';
+    destination[length] = '\0';
 
-    return copy;
+    return destination;
 }
+
 char* readLineFrom(FILE* in)
 {
-    return NULL;
-}
-char* getFilename(char* path){
-     size_t size = 0;
-    char* p;
+    const int readSize = 1024;
+    char* destination;
 
-    /** Check if it is only the name or a path  **/
+    destination = (char*) malloc(readSize * sizeof(char));
+
+    if(!destination)
+    {
+        return NULL;
+    }
+
+    destination = fgets(destination, readSize - 1, in);
+    if(destination)
+    {
+        char *pos;
+        if ((pos=strchr(destination, '\n')) != NULL)
+        {
+            *pos = '\0';
+        }
+        else
+        {
+            destination[readSize] = '\0';
+        }
+    }
+
+    return destination;
+}
+
+char* getname(char* path)
+{
+    size_t size = 0;
+    char* p;
     if(!strchr(path, '\\'))
     {
         return path;
     }
 
-    /** Find the begin of the name **/
     p = &path[strlen(path) - 1];
     for(; *p != '\\'; p--)
     {
         ++size;
     }
-
-    /** Return the name of the file **/
     return copyString(p + 1, size);
 }
-void cleanUp(char* filename, KEY key, char* input, char* output)
+
+void cleanUp(char* name, KEY key, char* input, char* output)
 {
-    if(filename)
+    if(name)
     {
-        free(filename);
+        free(name);
     }
     if(key.chars)
     {
@@ -64,22 +89,177 @@ void cleanUp(char* filename, KEY key, char* input, char* output)
         free(output);
     }
 }
+
 int exitWithError(char* msg)
 {
-    return 0;
+    fprintf(stderr, msg);
+    fprintf(stderr, "\n");
+
+    return EXIT_FAILURE;
 }
+
 int main(int argc, char** argv)
 {
-    char* filename = NULL;
+    int (*crypt) (KEY, const char*, char*);
+    char* name = NULL;
     KEY key;
     char* input = NULL;
     char* output = NULL;
+    int result = -1;
+    key.chars = NULL;
 
-    filename = getFilename(argv[0]);
+    name = getname(argv[0]);
 
-    if(!filename){
-        cleanUp(filename, key, input, output);
-        return exitWithError("");
+    if(!name)
+    {
+        cleanUp(name, key, input, output);
+        return exitWithError("Error: Failed to allocate memory");
     }
+    else if(strcmp(name, "encrypt.exe") == 0 || strcmp(name, "encrypt") == 0)
+    {
+        crypt = encrypt;
+    }
+    else if(strcmp(name, "decrypt.exe") == 0 || strcmp(name, "decrypt") == 0)
+    {
+        crypt = decrypt;
+    }
+    else
+    {
+        cleanUp(name, key, input, output);
+        return exitWithError("Error: Unsupported name");
+    }
+
+    if(argc == 2 || argc == 3)
+    {
+        key.chars = copyString(argv[1], strlen(argv[1]));
+
+        if(key.chars == NULL)
+        {
+            cleanUp(name, key, input, output);
+            return exitWithError("Error: Failed to allocate memory");
+        }
+
+        if(argc == 2)
+        {
+            input = readLineFrom(stdin);
+
+            if(!input)
+            {
+                cleanUp(name, key, input, output);
+                return exitWithError("Error: An input error occured");
+            }
+
+            output = (char*) malloc((strlen(input) + 1) * sizeof(char));
+
+            if(!output)
+            {
+                cleanUp(name, key, input, output);
+                return exitWithError("Error: Failed to allocate memory");
+            }
+            output[strlen(input)] = '\0';
+
+            result = (*crypt) (key, input, output);
+
+            if(!result)
+            {
+                printf("%s\n", output);
+            }
+        }
+        else
+        {
+            FILE* file = NULL;
+            file = fopen(argv[2], "r");
+
+            if(!file)
+            {
+                cleanUp(name, key, input, output);
+                return exitWithError("Error: Failed to open file");
+            }
+
+            do {
+                input = readLineFrom(file);
+
+                /** Check if it means an error, or end of file out of the loop **/
+                if(!input)
+                {
+                    break;
+                }
+
+                if(output)
+                {
+                    free(output);
+                }
+
+                output = (char*) malloc((strlen(input) + 1) * sizeof(char));
+
+                if(!output)
+                {
+                    fclose(file);
+                    cleanUp(name, key, input, output);
+                    return exitWithError("Error: Failed to allocate memory");
+                }
+                output[strlen(input)] = '\0';
+
+                result = (*crypt) (key, input, output);
+
+                if(!result)
+                {
+                    printf("%s\n", output);
+                }
+                else
+                {
+                    break;
+                }
+            } while(1);
+
+            /** Check if we reach the end of the file, or the encryption returned an error **/
+            if(!result && !feof(file))
+            {
+                fclose(file);
+                cleanUp(name, key, input, output);
+                return exitWithError("Error: Failed to read from file");
+            }
+
+            fclose(file);
+        }
+
+        if(result == 0)
+        {
+            /*All done, free the memory we allocated*/
+            cleanUp(name, key, input, output);
+        }
+        else if(result == E_KEY_TOO_SHORT)
+        {
+            cleanUp(name, key, input, output);
+            return exitWithError("Error: Key is to short");
+        }
+        else if(result == E_KEY_ILLEGAL_CHAR)
+        {
+            cleanUp(name, key, input, output);
+            return exitWithError("Error: Key contains illegal characters");
+        }
+        else if(result == E_MESSAGE_ILLEGAL_CHAR)
+        {
+            cleanUp(name, key, input, output);
+            return exitWithError("Error: Message contains illegal characters");
+        }
+        else if(result == E_CYPHER_ILLEGAL_CHAR)
+        {
+            cleanUp(name, key, input, output);
+            return exitWithError("Cypher text contains illegal characters");
+        }
+        else
+        {
+            cleanUp(name, key, input, output);
+            return exitWithError("Error: An unknown error has occurred");
+        }
+    }
+    else
+    {
+        cleanUp(name, key, input, output);
+        return exitWithError("Usage: KEY [file name]");
+    }
+
     return 0;
 }
+
--- crypter/testCrypto.c
+++ crypter/testCrypto.c
@@ -4,3 +4,4 @@
 #include <stdio.h>
 #include <string.h>
 
+//TODO: alles machen
